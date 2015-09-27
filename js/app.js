057cd258edfd259b69c1e69054106758edc50727
Joanna Gammel <jgammel@Joannas-MacBook-Pro.local>
2015-09-27 16:04:43 -0400

    regular expressions

--- js/app.js
+++ js/app.js
@@ -10,23 +10,16 @@ $(document).ready(function(){
     return false;
   });
 
-  function zombify(){
-    // 1. lower-case "r" at the end of words replaced with "rh".
-    // 2. an "a" or "A" by itself will be replaced with "hra".
-    // 3. the starts of sentences are capitalised (the "start of a sentence" is any occurrence of
-    //   ".!?", followed by a space, followed by a letter.)
-    // 4. "e" or "E" is replaced by "rr"
-    // 5. "i" or "I" is replaced by "rrRr"
-    // 6. "o" or "O" is replaced by "rrrRr"
-    // 7. "u" or "U" is replaced by "rrrrRr"
-    // 8. "r" or "R' is replaced by "RR"
+  function zombify(){   
     var input = $('#english').val();
-    input = input.replace(/r$/g, "rh");
-    input = input.replace(/a/gi, "hra");
-    input = input.replace(/e/gi, "rr");
-    input = input.replace(/i/gi, "rrRr");
-    input = input.replace(/o/gi, "rrrRr");
-    input = input.replace(/u/gi, "rrrrRr");
+    input = input.replace(/(r$)|(r\s)/g, "rh"); // 1. lower-case "r" at the end of words replaced with "rh".
+	input = input.replace(/a\b/ig, "hra"); // 2. an "a" or "A" by itself will be replaced with "hra".
+	input = input.replace(/[.!?]\s/g, "TODO");  // 3. the starts of sentences are capitalised (the "start of a sentence" is any occurrence of ".!?", followed by a space, followed by a letter.)
+    input = input.replace(/e/ig, "rr");  // 4. "e" or "E" is replaced by "rr"
+    input = input.replace(/i/ig, "rrRr"); // 5. "i" or "I" is replaced by "rrRr"
+    input = input.replace(/o/ig, "rrrRr"); // 6. "o" or "O" is replaced by "rrrRr"
+    input = input.replace(/u/ig, "rrrrRr"); // 7. "u" or "U" is replaced by "rrrrRr"
+    input = input.replace(/r/ig, "RR"); // 8. "r" or "R' is replaced by "RR"
     $('#zombie').val(input);
   }
 
